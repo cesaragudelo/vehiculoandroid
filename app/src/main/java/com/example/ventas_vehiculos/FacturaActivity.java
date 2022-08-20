58fd7ab9abca175f31aab935259deb6712380928
Juan Estiven <juan.z1997@hotmail.com>
2022-08-19 20:59:05 -0500

    clase 19-08-2022

---
+++ app/src/main/java/com/example/ventas_vehiculos/FacturaActivity.java
@@ -0,0 +1,20 @@
+package com.example.ventas_vehiculos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class FacturaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_factura);
+    }
+    public void Regresar(View view){
+        Intent intmain=new Intent(this,MainActivity.class);
+        startActivity(intmain);
+    }
+}
---
+++ app/src/main/java/com/example/ventas_vehiculos/VehiculoActivity.java
@@ -0,0 +1,81 @@
+package com.example.ventas_vehiculos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class VehiculoActivity extends AppCompatActivity {
+    EditText jetplaca,jetmarca,jetmodelo,jetvalor;
+    Switch jetactivovhei;
+    long resp;
+    String placa,marca,modelo,valor;
+    ClsOpenHelper admin=new ClsOpenHelper(this,"concesionario.db",null,1);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehiculo);
+        //
+        getSupportActionBar().hide();
+        jetplaca=findViewById(R.id.etplaca1);
+        jetmarca=findViewById(R.id.etmarca1);
+        jetmodelo=findViewById(R.id.etmodelo1);
+        jetvalor=findViewById(R.id.etvalor1);
+        jetactivovhei=findViewById(R.id.swactivo1);
+    }
+    public void Guardar(View view){
+
+        placa=jetplaca.getText().toString();
+        marca=jetmarca.getText().toString();
+        modelo=jetmodelo.getText().toString();
+        valor=jetvalor.getText().toString();
+        if(placa.isEmpty() || marca.isEmpty()||
+        modelo.isEmpty() || valor.isEmpty()){
+            Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+            jetplaca.requestFocus();
+        }
+        else {
+            SQLiteDatabase db=admin.getWritableDatabase();
+            ContentValues registro=new ContentValues();
+           registro.put("placa",placa);
+            registro.put("marca",marca);
+            registro.put("modelo",modelo);
+            registro.put("valor",Integer.parseInt(valor));
+            resp=db.insert("TblVehiculo",null,registro);
+            if (resp > 0){
+                Toast.makeText(this, "Registro guardado", Toast.LENGTH_SHORT).show();
+                Limpiar_campos();
+            }
+            else
+                Toast.makeText(this, "Error guardando registro", Toast.LENGTH_SHORT).show();
+            db.close();
+
+
+        }
+    }
+
+
+    private void Limpiar_campos(){
+        jetplaca.setText("");
+        jetvalor.setText("");
+        jetmodelo.setText("");
+        jetmarca.setText("");
+        jetactivovhei.setChecked(false);
+        jetplaca.requestFocus();
+    }
+
+
+
+    public void Regresar(View view){
+        Intent intmain=new Intent(this,MainActivity.class);
+        startActivity(intmain);
+    }
+}
